60748867207443cb70f18e3c6ee9acae3423c050
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2021-09-03 15:07:49 +0100

    Display emails in lowercase

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -9,6 +9,6 @@ export const Card = (props) => (
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
     <h2>{props.monster.name}</h2>
-    <p>{props.monster.email}</p>
+    <p>{props.monster.email.toLowerCase()}</p>
   </div>
 );
